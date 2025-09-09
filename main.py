61726ec07ee008ca17c4596866574b355ef72b3c
Leonidig <leonid200955@gmail.com>
2025-09-09 20:51:06 +0300

    lesson1

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('Meteorite_Landings.csv')
+# print(df.head(10))
+# print(df.info())
+# print(df.columns)
+
+# a = len(df)
+# print(a)
+# print(df.describe())
